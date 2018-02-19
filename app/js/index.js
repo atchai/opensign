f31af377d1f6a6a00600cd0f2639d1168b48775f
John Griffin <john@atchai.com>
2018-02-19 10:51:27 +0000

    Improve UX and add page to countersign doc

--- app/js/index.js
+++ app/js/index.js
@@ -3,6 +3,17 @@ var addToLog = function(id, txt) {
   $(id).append("<br>" + txt);
 };
 
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
+
 
 // ===========================
 // Blockchain example
@@ -12,12 +23,31 @@ $(document).ready(function() {
   var ipfs = ''
   
   $("#blockchain").hide();
+  $("#invite").hide();
+  
+  console.log(getQueryVariable("ipfs"))
+  $("span.file_url").html('<a href="' + EmbarkJS.Storage.getUrl(getQueryVariable("ipfs")) + '">this document</a>');
+  
+  $("#sign button.set").click(function() {
+    let value = web3.utils.asciiToHex(getQueryVariable("ipfs"))
+    
+    Notary.methods.signDocument(value).send({from: web3.eth.defaultAccount});
+        
+    $("#signed").show();
+    $("#signed").append("<p>You have signed! Keep a record of this URL to refer to your agreement.</p>");
+    
+  });
 
   $("#blockchain button.set").click(function() {
+    let value = web3.utils.asciiToHex(ipfs)
     
-    let value = "0x23"
     Notary.methods.addDocument(value).send({from: web3.eth.defaultAccount});
-    addToLog("#invite", "You can ask others to sign the same agreement by going to this address: ");
+    
+    let sign_url = window.location.href + "sign.html?ipfs=" + ipfs;
+    
+    $("#invite").show();
+    $("#invite").append("<p>Keep this address and share it with others who you want to sign the document: </p>");
+    $("#invite").append('<p><a href="' + sign_url + '">' + sign_url + "</p>");
     
   });
   
@@ -25,10 +55,11 @@ $(document).ready(function() {
   $("#storage button.uploadFile").click(function() {
     var input = $("#storage input[type=file]");
     EmbarkJS.Storage.uploadFile(input).then(function(hash) {
-      $("span.fileIpfsHash").html(hash);
-      $("input.fileIpfsHash").val(hash);
       ipfs = hash;
+      let url = EmbarkJS.Storage.getUrl(ipfs);
+      $("#storage").append('<p>Your document is accessible here: <a href="' + url + '">' + url + "</p>");
       $("#blockchain").show()
+      
     })
     .catch(function(err) {
       if(err){

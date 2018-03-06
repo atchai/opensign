8397a47501193d7b5d8a857aa0fb6b2763c7ee68
John Griffin <john@atchai.com>
2018-03-06 10:38:59 +0000

    Added warnings for metamask.  Using infura for IPFS

--- app/js/index.js
+++ app/js/index.js
@@ -1,7 +1,37 @@
-
-var addToLog = function(id, txt) {
-  $(id).append("<br>" + txt);
-};
+// Check for Metamask and show/hide appropriate warnings.
+window.addEventListener('load', function() {
+  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+  if ((typeof web3 !== 'undefined') && (web3.givenProvider !== null)) {
+        // Use Mist/MetaMask's provider    
+    web3.eth.getAccounts(function(err, accounts){
+        if (err != null) console.error("An error occurred: "+err);
+        
+        // User is not logged into Metamask
+        else if (accounts.length == 0) {
+          $("#main-content").hide();
+          $("#metamask-login").show();
+          $("#metamask-install").hide();
+          console.log("User is not logged in to MetaMask");
+        }
+      
+        // User is logged in to Metamask
+        else {
+          console.log("User is logged in to MetaMask");
+          $("#main-content").show();
+          $("#metamask-login").hide();
+          $("#metamask-install").hide();
+        }
+      
+    });
+    
+  // User does not have Metamask / web3 provider  
+  } else {
+    $("#main-content").hide();
+    $("#metamask-login").hide();
+    $("#metamask-install").show();
+    console.log('No web3? You should consider trying MetaMask!')
+  }
+})
 
 var getQueryVariable = function(variable)
 {
@@ -15,9 +45,6 @@ var getQueryVariable = function(variable)
 }
 
 
-// ===========================
-// Blockchain example
-// ===========================
 $(document).ready(function() {
   
   var ipfs = ''
@@ -26,7 +53,12 @@ $(document).ready(function() {
   $("#invite").hide();
   $("#learn-more").hide();
   
- // EmbarkJS.Storage.setProvider('ipfs',{server: 'ipfs.infura.io', port: '5001'})
+  EmbarkJS.Storage.setProvider('ipfs',{server: 'ipfs.infura.io', port: '5001', protocol: 'https'})  
+  
+  $(".btn-learn-more").click(function() {
+    $(".btn-learn-more").hide();
+    $("#learn-more").show();
+  });
   
   // Sign page
   if (getQueryVariable("ipfs") != false) {
@@ -59,13 +91,25 @@ $(document).ready(function() {
   }
   
   
-  $(".btn-learn-more").click(function() {
-    $(".btn-learn-more").hide();
-    $("#learn-more").show();
+  // Step 1 - upload file to IPFS
+  $("#storage button.uploadFile").click(function() {
+    var input = $("#storage input[type=file]");
+    EmbarkJS.Storage.uploadFile(input).then(function(hash) {
+      ipfs = hash;
+      let url = EmbarkJS.Storage.getUrl(ipfs);
+      $("#storage .card-body").append('<p><a href="' + url + '">Here is a link to your document</p>"');
+      $("#blockchain").show()
+      
+    })
+    .catch(function(err) {
+      if(err){
+        $("#storage .card-body").append('<p>Sorry, it looks like there is a problem connecting to IPFS at the moment. Please try again later.</p>');
+        console.log("IPFS uploadFile Error => " + err.message);
+      }
+    });
   });
   
-  
-
+  // Step 2 - sign document
   $("#blockchain button.set").click(function() {
     let value = web3.utils.asciiToHex(ipfs)
     
@@ -77,23 +121,6 @@ $(document).ready(function() {
     $("#invite .card-body").append("<p>Keep this address and share it with others who you want to sign the document: </p>");
     $("#invite .card-body").append('<p><a href="' + sign_url + '">' + sign_url + "</p>");
     
-  });
-  
-
-  $("#storage button.uploadFile").click(function() {
-    var input = $("#storage input[type=file]");
-    EmbarkJS.Storage.uploadFile(input).then(function(hash) {
-      ipfs = hash;
-      let url = EmbarkJS.Storage.getUrl(ipfs);
-      $("#storage .card-body").append('<p>Your document is accessible here: <a href="' + url + '">' + url + "</p>");
-      $("#blockchain").show()
-      
-    })
-    .catch(function(err) {
-      if(err){
-        console.log("IPFS uploadFile Error => " + err.message);
-      }
-    });
   });
 
 });
